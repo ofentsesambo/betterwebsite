3ef2b6924020ffa3c9ccdf266d55c4a8b9b6dcdd
ofentsesambo <ofentsesambo@gmail.com>
2020-07-27 21:46:12 +0200

    cant figure out what the error is

--- script.js
+++ script.js
@@ -58,3 +58,67 @@ logo.addEventListener('click', function(){
     handleOpacity(main, about, skills, contact);
     handleAnimation(mainH1, mainH12, mainPara, mainAnker);
 })
+
+
+
+
+let logo = document.querySelector("#logo");
+let aboutLink = document.querySelector("#aboutLink");
+let skillsLink = document.querySelector("#skillsLink");
+let contactLink = document.querySelector("#contactLink");
+
+
+let main = document.querySelector("#main");
+let about = document.querySelector("#about");
+let skills = document.querySelector("#skills");
+let contact = document.querySelector("#contact");
+
+let mainH1 = document.querySelector("#main h1");
+let mainH12 = document.querySelector("#main h1:nth-child(2)");
+let mainPara = document.querySelector("#main p");
+let mainAnker = document.querySelector("#main a");
+
+let aboutH1 = document.querySelector("#about h1");
+let aboutPara = document.querySelector("#about p");
+let aboutAnker = document.querySelector("#about a");
+
+let skillsH1 = document.querySelector("skills h1");
+let skillsPara = document.querySelector("#skill p");
+let skillsCard = document.querySelector("#skills .skills-card");
+
+let contactH1 = document.querySelector("#contact h1");
+let contactForm = document.querySelector("#contact form");
+let contactAnker = document.querySelector("#contact a");
+
+function handleOpacity(opacityOne, opacityZero1, opacityZero2,opacityZero3){
+    opacityOne.style.opacity = 1;
+    opacityZero1.style.opacity = 0;
+    opacityZero2.style.opacity = 0;
+    opacityZero3.style.opacity = 0;
+}
+
+function handleAnimation() {
+    for(let i=0; i<arguments.length; i++) {
+        arguments[i].classList.remove('main-animation')
+        arguments[i].offsetWidth;
+        arguments[i].classList.add(main-animation)
+    }
+}
+
+logo.addEventListener('click', function(){
+    handleOpacity(main, about, skills, contact);
+    handleAnimation(mainH1, mainH12, mainPara, mainAnker);
+})
+
+logo.addEventListener('click', function(){
+    handleOpacity(main, about, skills, contact);
+    handleAnimation(mainH1, mainH12, mainPara, mainAnker);
+})
+logo.addEventListener('click', function(){
+    handleOpacity(main, about, skills, contact);
+    handleAnimation(mainH1, mainH12, mainPara, mainAnker);
+})
+logo.addEventListener('click', function(){
+    handleOpacity(main, about, skills, contact);
+    handleAnimation(mainH1, mainH12, mainPara, mainAnker);
+})
